8b35dafbd0e579fffd1e1c1014ba72a22d0870c4
emreysalli <94170595+emreysalli@users.noreply.github.com>
2022-06-29 23:49:54 +0300

    added filtered

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Task } from "./components/Task.js";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import CheckIcon from '@mui/icons-material/Check';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Button from '@mui/material/Button';
+import CheckIcon from "@mui/icons-material/Check";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+
 function App() {
   //css
   const sectionTitle = {
@@ -35,67 +36,63 @@ function App() {
     margin: "5px",
     fontSize: "24px",
   };
-  const itemBox = {
-    backgroundColor: "#FFFFFF",
-    padding: 8,
-    borderRadius: 10,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-    color: "#fff",
-    marginTop: 30,
-    display: "flex",
-  };
 
-  document.title="TODO LIST";
+  document.title = "TODO LIST";
   const [task, setTask] = useState("");
   const [taskItems, setTaskItems] = useState([]);
+  const [filteredTask, setFilteredTask] = useState([]);
   const [priority, setPriority] = useState("");
-  const priorityDict={"Low":"green","Medium":"yellow","High":"orange","Immediately":"red"}; 
+  const priorityDict = {
+    Low: "green",
+    Medium: "yellow",
+    High: "orange",
+    Immediately: "red",
+  };
 
   const handleChange = (event) => {
     setPriority(event.target.value);
   };
 
-  const handleAddTask = event => {
+  const handleAddTask = (event) => {
     event.preventDefault();
     if (task !== "") {
-      let taskDict={text:task, isComplete:false, taskPriority:priority};
+      let taskDict = { text: task, isComplete: false, taskPriority: priority };
       setTaskItems([...taskItems, taskDict]);
       setPriority("");
       setTask("");
     }
   };
 
-  const completedTask=(index)=>{
+  const completedTask = (index) => {
     let itemsCopy = [...taskItems];
 
-    itemsCopy[index].isComplete=!itemsCopy[index].isComplete;
-    
+    itemsCopy[index].isComplete = !itemsCopy[index].isComplete;
+
     setTaskItems(itemsCopy);
-  }
+  };
 
   const deleteTask = (index) => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index, 1);
     setTaskItems(itemsCopy);
   };
-  var filtered=taskItems;
-  const allClick=()=>{
-    filtered=taskItems;
-  }
-  const activeClick=()=>{
-    filtered=taskItems.filter((item)=>{
-      return item.isComplete===false;
-    })
-  }
 
-  const completedClick=()=>{
-    filtered=taskItems.filter((item)=>{
-      return item.isComplete===true;
-    })
-  }
+  const filtered = (complete) => {
+    if (complete === null) {
+      setFilteredTask(taskItems);
+      return;
+    }
+
+    const filtered = taskItems.filter((item) => {
+      return item.isComplete === complete;
+    });
+
+    setFilteredTask(filtered);
+  };
+
+  useEffect(() => {
+    setFilteredTask(taskItems);
+  }, [taskItems]);
 
   return (
     <div className="App">
@@ -103,7 +100,7 @@ function App() {
       <form onSubmit={handleAddTask}>
         <input
           style={input}
-          placeholder={"Write a task"}
+          placeholder={"  Write a task"}
           value={task}
           onChange={(event) => setTask(event.target.value)}
         />
@@ -111,29 +108,62 @@ function App() {
           +
         </button>
         <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
           value={priority}
           onChange={handleChange}
           label="Priority"
         >
-        {Object.keys(priorityDict).map(function(key, index){
-          return (
-            <MenuItem value={key} key={index}>{key}</MenuItem>
-          );
-        })}
-      </Select>
+          {Object.keys(priorityDict).map(function (key, index) {
+            return (
+              <MenuItem value={key} key={index}>
+                {key}
+              </MenuItem>
+            );
+          })}
+        </Select>
       </form>
-      <ButtonGroup style={{display:"flex",justifyContent:"flex-start",margin:"10px 0px"}} size="medium" variant="outlined" aria-label=" button group">
-        <Button style={{color:"black", borderColor:"black"}} type="submit" onClick={() => allClick()}>All</Button>
-        <Button style={{color:"black", borderColor:"black"}} type="submit" onClick={() => activeClick()}>Active</Button>
-        <Button style={{color:"black", borderColor:"black"}} type="submit" onClick={() => completedClick()}>Completed</Button>
+      <ButtonGroup
+        className="filterBtnGroup"
+        size="medium"
+        variant="outlined"
+        exclusive
+        aria-label="outlined button group"
+      >
+        <Button
+          style={{ color: "black", borderColor: "black" }}
+          className="filterBtn"
+          onClick={() => filtered(null)}
+        >
+          All
+        </Button>
+        <Button
+          style={{ color: "black", borderColor: "black" }}
+          className="filterBtn"
+          onClick={() => filtered(false)}
+        >
+          Active
+        </Button>
+        <Button
+          style={{ color: "black", borderColor: "black" }}
+          className="filterBtn"
+          onClick={() => filtered(true)}
+        >
+          Completed
+        </Button>
       </ButtonGroup>
 
-      {filtered.map((item, index) => {
+      {filteredTask.map((item, index) => {
         return (
-          <div style={{...itemBox, backgroundColor:priorityDict[item.taskPriority]}} key={index} id={index}>
-            <Task text={item.text} id={index} complete={item.isComplete}/>
+          <div
+            style={{
+              backgroundColor: priorityDict[item.taskPriority],
+            }}
+            className="itemBox"
+            key={index}
+            id={index}
+          >
+            <Task text={item.text} id={index} complete={item.isComplete} />
             <div className="buttons">
               <IconButton
                 aria-label="delete"
@@ -150,11 +180,10 @@ function App() {
                 <DeleteIcon />
               </IconButton>
             </div>
-          
           </div>
         );
       })}
     </div>
   );
-};
+}
 export default App;
