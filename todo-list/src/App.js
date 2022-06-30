136508f2a74b6c1df786ed21892970a54847f6e7
emreysalli <94170595+emreysalli@users.noreply.github.com>
2022-06-30 13:54:08 +0300

    updated

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -10,34 +10,9 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 
 function App() {
-  //css
-  const sectionTitle = {
-    fontSize: 36,
-    fontWeight: "bold",
-  };
-  const input = {
-    backgroundColor: "#fff",
-    borderRadius: 60,
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-    width: 250,
-    height: 40,
-    fontSize: "16px",
-  };
-  const addWrapper = {
-    width: 40,
-    height: 40,
-    backgroundColor: "#fff",
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-    margin: "5px",
-    fontSize: "24px",
-  };
 
   document.title = "TODO LIST";
+  
   const [task, setTask] = useState("");
   const [taskItems, setTaskItems] = useState([]);
   const [filteredTask, setFilteredTask] = useState([]);
@@ -120,15 +95,15 @@ function App() {
 
   return (
     <div className="App">
-      <h2 style={sectionTitle}>TODO LIST</h2>
+      <h2 className="sectionTitle">TODO LIST</h2>
       <form onSubmit={handleAddTask}>
         <input
-          style={input}
-          placeholder={"  Write a task"}
+          className="input"
+          placeholder={"Write a task"}
           value={task}
           onChange={(event) => setTask(event.target.value)}
         />
-        <button type="submit" style={addWrapper}>
+        <button type="submit" className="addBtn">
           +
         </button>
         <Select
